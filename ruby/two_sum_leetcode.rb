68c19ffb97c91de9c084c4f1a30245cf48a25962
Alaska01 <aadaniel108@gmail.com>
2022-04-04 15:54:25 +0100

    Start two sum problem

---
+++ ruby/two_sum_leetcode.rb
@@ -0,0 +1,48 @@
+=begin
+********************************************************************************
+https://leetcode.com/problems/two-sum/
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+***********************************************************************************
+=end
+
+def two_sum(nums, target)
+    
+    a = nums.size
+
+    b=(0...a).to_a
+
+    
+    
+    nums.each_with_index do |val, index|
+       
+        if(num.size > 1 && index > 0)
+              num  
+
+        end
+            
+
+    end
+    
+end
+
+two_sum([2,7,11,15], 9)
