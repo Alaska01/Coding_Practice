e5a98e8174a3362c0bf3f8f6cd654254a98649dd
Alaska01 <aadaniel108@gmail.com>
2022-04-08 17:28:29 +0100

    Conversion From Base ten to three also known as ternery

--- javascript/array_methods_coderbyte2.js
+++ javascript/array_methods_coderbyte2.js
@@ -1,4 +1,42 @@
 /*
+simply convert to base three from base ten i.e input 21, output = 110
 This link is an expanation https://en.wikipedia.org/wiki/Ternary_numeral_system
 */
 
+function mathsChallenge(num){
+
+  let remainder =[]
+  let loopLength = num/ 3;
+
+  if(num < 3){
+    return remainder.push(num);
+  }
+
+  currentNum = num;
+  let i =0;
+  
+  while (i < num/3 ){
+    if(currentNum% 3==0 ){
+
+      remainder.push(0)
+      currentNum = currentNum/3
+      
+    }
+
+    if(currentNum % 3 != 0  && currentNum > 3){
+        remainder.push(currentNum % 3)
+        currentNum= Math.floor((currentNum/3))
+    }
+
+    if(currentNum < 3){
+      remainder.push(currentNum)
+      break;
+    }
+    i++;
+  }
+
+
+  return remainder.reverse();
+}
+
+console.log(mathsChallenge(100))
